6a8dc00f022c9f3af9815222a37eefaec9767e4d
Yash Mittal <mittal.yash12@hotmail.com>
2023-09-08 18:43:07 +0530

    feat: add revocation functions

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,23 @@ import { CredentialsModule } from './credentials/credentials.module';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthCheckUtilsService } from './credentials/utils/healthcheck.utils.service';
 import { PrismaClient } from '@prisma/client';
+import { RevocationListService } from './revocation-list/revocation-list.service';
+import { RevocationListModule } from './revocation-list/revocation-list.module';
+import { UtilsModule } from './utils/utils.module';
+import { RevocationList } from './revocation-list/revocation-list.helper';
+import { RevocationListImpl } from './revocation-list/revocation-list.impl';
+import { IdentityUtilsService } from './credentials/utils/identity.utils.service';
 
 @Module({
   imports: [
     HttpModule,
     ConfigModule.forRoot({ isGlobal: true }),
     CredentialsModule,
-    TerminusModule
+    TerminusModule,
+    RevocationListModule,
+    UtilsModule
   ],
   controllers: [AppController],
-  providers: [AppService, ConfigService, PrismaClient, HealthCheckUtilsService],
+  providers: [AppService, ConfigService, PrismaClient, HealthCheckUtilsService, RevocationListService, RevocationList, RevocationListImpl, IdentityUtilsService],
 })
 export class AppModule {}
